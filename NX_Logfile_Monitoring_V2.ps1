35bab27958867cc718919282f95480c07292f026
PLMBakery <97092180+PLMBakery@users.noreply.github.com>
2023-10-26 15:50:55 +0200

    release of two logfile monitoring scripts
    These scripts are to monitor logfiles of TC RAC user and NX Logfile. Sometimes it is need to trail what happens in the system

---
+++ NX_Logfile_Monitoring_V2.ps1
@@ -0,0 +1,46 @@
+﻿# copyright by PLM Baker
+#
+# 2023
+#
+#
+# This script is for monitoring the NX Logfile
+
+
+
+$loggedInUsername = $env:USERNAME
+$folderPath = "D:\[path to logfile]"
+$fileNamePattern = "$loggedInUsername*.syslog"
+
+# Function to get the latest log file
+function Get-LatestLogFile {
+    param (
+        [string]$folderPath,
+        [string]$fileNamePattern
+    )
+
+    # Get all log files with the specified criteria
+    $logFiles = Get-ChildItem -Path $folderPath -Filter $fileNamePattern | Sort-Object LastWriteTime -Descending
+
+    if ($logFiles.Count -gt 0) {
+        return $logFiles[0].FullName
+    }
+    
+    return $null
+}
+
+while ($true) {
+    # Get the latest log file
+    $latestLogFile = Get-LatestLogFile $folderPath $fileNamePattern
+
+    if (-not $latestLogFile) {
+        Write-Host "No log files found with the specified criteria."
+        exit
+    }
+
+    # Monitor the latest log file
+    Write-Host "Monitoring the latest log file: $latestLogFile"
+    Get-Content -Path $latestLogFile -Wait
+
+    # Wait for 5 minutes before checking for a new log file
+    Start-Sleep -Seconds 300
+}
---
+++ TC_Rac_Logfile_Monitoring_V2.ps1
@@ -0,0 +1,44 @@
+﻿# copyright by PLM Baker
+#
+# 2023
+#
+#
+# This script is for monitoring the NX Logfile
+
+
+$loggedInUsername = $env:USERNAME
+#you have to fit the next line to your installation
+$folderPath = "D:\[path to logfile]\$loggedInUsername\Teamcenter\RAC-4T\TC13\"
+$fileNamePattern = "$loggedInUsername*.log"
+
+# Function to get the latest log file
+function Get-LatestLogFile {
+    param (
+        [string]$folderPath,
+        [string]$fileNamePattern
+    )
+
+    # Get all log files with the specified criteria
+    $logFiles = Get-ChildItem -Path $folderPath -Filter $fileNamePattern | Sort-Object LastWriteTime -Descending
+
+    if ($logFiles.Count -gt 0) {
+        return $logFiles[0].FullName
+    }
+    
+    return $null
+}
+
+# Get the latest log file
+$latestLogFile = Get-LatestLogFile $folderPath $fileNamePattern
+
+if (-not $latestLogFile) {
+    Write-Host "No log files found with the specified criteria."
+    exit
+}
+
+# Monitor the latest log file
+Write-Host "Monitoring the latest log file: $latestLogFile"
+Get-Content -Path $latestLogFile -Wait
+
+ # Wait for 5 minutes before checking for a new log file
+    Start-Sleep -Seconds 300
